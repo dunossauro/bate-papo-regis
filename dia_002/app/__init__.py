2a05f0f7eb7b04427a4699cda594b8b766a56a9c
dunossauro <mendesxeduardo@gmail.com>
2021-06-18 09:29:27 -0300

    Segundo pate papo :fire:

---
+++ dia_002/app/__init__.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+
+class UserSchema(BaseModel):
+    nome: str
+    email: str
+
+
+def create_app():
+    # Application factory
+    app = FastAPI()
+
+    from .database import session, User
+    from sqlalchemy.future import select
+
+    @app.post('/user/add/', status_code=201)
+    async def create_user(user: UserSchema):
+        async with session() as s:
+            # Todas as operações do banco vão rolar aqui!
+            s.add(
+                User(nome=user.nome, email=user.email)
+            )
+            await s.commit()
+
+        return user
+
+    @app.patch('/user/{user_id}/', status_code=200)
+    async def patch_user(user: UserSchema, user_id):
+        async with session() as s:
+            query = await s.execute(
+                select(User).where(User.id == user_id)
+            )
+
+            result = query.scalar()
+
+            if result:
+                result.nome = user.nome
+                result.email = user.email
+                await s.commit()
+
+        if not result:
+            raise HTTPException(
+                status_code=404, detail=f'Not Found {user_id}'
+            )
+
+        return {'message': str(result)}
+
+    return app
