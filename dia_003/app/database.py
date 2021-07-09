6e3c6e01046659d966933d722e86b204c78d29c2
dunossauro <mendesxeduardo@gmail.com>
2021-07-08 22:08:29 -0300

    papo 003

---
+++ dia_003/app/database.py
@@ -0,0 +1,41 @@
+# Core
+# ORM <-
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import (
+    declarative_base, sessionmaker
+)
+from sqlalchemy.ext.asyncio import (
+    AsyncSession, create_async_engine
+)
+
+engine = create_async_engine(
+    # Isso vire um parâmetro do BaseSettings do pydantic
+    'sqlite+aiosqlite:///./db.db'
+)
+session = sessionmaker(
+    engine,
+    expire_on_commit=True,
+    class_=AsyncSession
+)  # 1.4 +
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    nome = Column(String)
+    email = Column(String)
+
+
+async def create_database():
+    # Conecta no banco
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+if __name__ == '__main__':
+    from asyncio import run
+    run(create_database())
