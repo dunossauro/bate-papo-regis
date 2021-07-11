da17baeb049590ecf19e2a22593b0f10e74da8b4
Regis Santos <regis42santos@gmail.com>
2021-07-11 06:58:53 -0300

    Finaliza a implementação dos métodos para post e patch.

--- dia_002_rg/app/__init__.py
+++ dia_002_rg/app/__init__.py
@@ -16,10 +16,34 @@ def create_app():
 
     @app.post('/user/add/', status_code=201)
     async def create_user(user: UserSchema):
-        ...
+        async with session() as s:
+            # Todas as operações do banco vão rolar aqui!
+            s.add(
+                User(nome=user.nome, email=user.email)
+            )
+            await s.commit()
+
+        return user
 
     @app.patch('/user/{user_id}/', status_code=200)
     async def patch_user(user: UserSchema, user_id):
-        ...
+        async with session() as s:
+            query = await s.execute(
+                select(User).where(User.id == user_id)
+            )
+
+            result = query.scalar()
+
+            if result:
+                result.nome = user.nome
+                result.email = user.email
+                await s.commit()
+
+        if not result:
+            raise HTTPException(
+                status_code=404, detail=f'Not Found {user_id}'
+            )
+
+        return {'message': str(result)}
 
     return app
