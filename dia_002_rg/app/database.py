5d3a7a3306d72412877b444152951b4c1e69c8c2
Regis Santos <regis42santos@gmail.com>
2021-07-11 06:48:24 -0300

    Adiciona database.py

---
+++ dia_002_rg/app/database.py
@@ -0,0 +1,34 @@
+# ORM do SQLAlchemy
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+engine = create_async_engine('sqlite+aiosqlite:///./db.db')
+
+session = sessionmaker(
+    engine,
+    expire_on_commit=True,
+    class_=AsyncSession
+)  # 1.4 +
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    nome = Column(String)
+    email = Column(String)
+
+
+async def create_database():
+    # Conecta no banco
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+
+if __name__ == '__main__':
+    from asyncio import run
+    run(create_database())
